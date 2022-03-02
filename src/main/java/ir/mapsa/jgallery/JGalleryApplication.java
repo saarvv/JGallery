d2e0516afc0c845aa0a6bcaac7fa4b6d95567389
hrmemar2 <hrmemar2@gmail.com>
2022-03-02 22:07:01 +0330

    init project

---
+++ src/main/java/ir/mapsa/jgallery/JGalleryApplication.java
@@ -0,0 +1,13 @@
+package ir.mapsa.jgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JGalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JGalleryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/mapsa/jgallery/JGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mapsa.jgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JGalleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
