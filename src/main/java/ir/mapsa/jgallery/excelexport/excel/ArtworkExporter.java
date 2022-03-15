7ed55637aa9115ea838fd9cb5112cd1aa7a4f7ce
saarvv <sarvinahangian@yahoo.com>
2022-03-16 02:30:45 +0330

    excel

--- src/main/java/ir/mapsa/jgallery/excelexport/excel/ArtworkExporter.java
+++ src/main/java/ir/mapsa/jgallery/excelexport/excel/ArtworkExporter.java
@@ -2,16 +2,18 @@ package ir.mapsa.jgallery.excelexport.excel;
 
 import ir.mapsa.jgallery.artist.Artist;
 import ir.mapsa.jgallery.artwork.Artwork;
+import ir.mapsa.jgallery.artwork.ArtworkService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExcelFileWriterTest {
+    ArtworkService artworkService
      ExcelExporter exporter=new ExcelExporter();
 
     List<Artwork> artworks=new ArrayList<>();
 
-    List<Artist> artists=new ArrayList<>();
+    //List<Artist> artists=new ArrayList<>();
 
 
 }
